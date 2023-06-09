5fb7a095b61fdaae8d4148626b4ca5cd9ea79e36
Arka Roy Chowdhury <32403012+arkaroy14@users.noreply.github.com>
2023-06-09 13:06:28 +0530

    Upload requirement script

---
+++ req.sh
@@ -0,0 +1,7 @@
+sudo pip3 install -U telegram-upload && sudo pip3 install -U pyrogram && sudo pip3 install -U TgCrypto
+sudo apt-get install mkvtoolnix
+sudo pip3 install moviepy
+
+#FOR telegram-upload check guide to move this 2 files in proper path if not present already.
+cp telegram-upload.json /root/.config/telegram-upload.json
+cp telegram-upload.session /root/.config/telegram-upload.session
