3c51c8b0aaf994bf03479be66b9867456bc96cb1
Arka Roy Chowdhury <32403012+arkaroy14@users.noreply.github.com>
2023-08-09 12:48:28 +0530

    req.sh: remove trace of telegram-upload

--- req.sh
+++ req.sh
@@ -1,7 +1,5 @@
-sudo pip3 install -U telegram-upload && sudo pip3 install -U pyrogram && sudo pip3 install -U TgCrypto
+sudo pip3 install -U pyrogram && sudo pip3 install -U TgCrypto
 sudo apt-get install mkvtoolnix
 sudo pip3 install moviepy
 
-#FOR telegram-upload check guide to move this 2 files in proper path if not present already.
-cp telegram-upload.json /root/.config/telegram-upload.json
-cp telegram-upload.session /root/.config/telegram-upload.session
+#FOR upload.py need to place name.session in that folder or it will create 1 session on first run later you can reuse that session
